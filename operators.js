151a41903f645e1fe4a31cc0f84c452b6553ea63
pranavsinggh <singh167pranav@gmail.com>
2025-01-24 20:27:57 +0530

    comparison op

---
+++ operators.js
@@ -0,0 +1,19 @@
+//! ==
+//? only checks for value, does not check the data type
+console.log(10 == 10);
+console.log(10 == "10")
+
+//! ===
+//? check the value, along with that check the data type also
+console.log(10 === 10)
+console.log(10 === "10")
+
+//! !=
+//? check the value only, not the data type
+console.log(10 != 10)
+console.log(10 != "10")
+
+//! !==
+//? check the data type along with the value
+console.log(10 !== 10)
+console.log(10 !== "10")
