4fe3f5745a4995ab60a5cf2fad4f259d8e6f4015
pranavsinggh <singh167pranav@gmail.com>
2025-01-29 21:39:56 +0530

    lexical closue currying generator

--- typesOfFunction.js
+++ typesOfFunction.js
@@ -7,7 +7,7 @@
 // greet();
 
 //! Anonymous function
-//? A function does not having a function identifier, WE need to store this inside a variable and execute it with the help of that variable
+//? A function does not having a function identifier, We need to store this inside a variable and execute it with the help of that variable
 // let a = function () {
 //   console.log("Hello i am executed from an anonymous function");
 // };
@@ -65,7 +65,7 @@
 // let l = () => ({ name: "Atul", age: 21 });
 // console.log(l());
 
-//TODO Difference between named function and arrow function
+//TODO Difference between function created with function keyword and arrow function
 //~ Function created with function keyword has more syntax, while arrow function has less syntax to follow.
 //~ Arrow function has implicit return
 //~ Named functions can be invoked before initialization while arrow function cant.
@@ -86,7 +86,7 @@
 // product(10, 20);
 
 //! IIFE - Immediate Invoke Function Expression
-//? It is a function which is excuted immediatley after it is defined.
+//? It is a function which is executed immediatley after it is defined.
 //? Syntax - (function (parameters){ })(arguments)
 //? It is a common way to prevent global scope pollution by creating private scope(local scope) for variables.
 //? At the end provide a semi-colon
@@ -123,11 +123,144 @@
 //     function c() {
 //       console.log("I am a child function");
 //     }
-//     c();
+//     return c;
 //   }
-//   p();
+//   // let x = p();
+//   // console.log(x)
+//   // x()
+//   return p;
 // }
-// gp();
+// let x = gp();
+// console.log(x);
+// let y = x();
+// console.log(y)
+// y()
 
-console.log(window)
-console.log(this)
+// console.log(window)
+// console.log(this)
+
+// let a = 10;
+// let b = 20;
+// let c = 30;
+// // console.log(a, b, c);
+
+// function grandParent() {
+//   let x = 100;
+//   let y = 200;
+//   let z = 300;
+// //   console.log("Global data " + a, b, c);
+//   console.log("From grand parent " + x, y, z);
+
+//   function parent() {
+//     let p = 1000;
+//     let q = 2000;
+//     let r = 3000;
+//     // console.log("Globally" + a, b, c);
+//     console.log("From grand parent " + x);
+//     console.log("From parent " + p, q, r);
+
+//     function child() {
+//       let e = 10000;
+//       let f = 20000;
+//       let g = 30000;
+//       console.log("Globally" + a, b, c);
+//       console.log("From grand parent " + x, y);
+//       console.log("From parent " + p, q, r);
+//       console.log("From child " + e, f, g);
+//     }
+//     child();
+//   }
+//   parent();
+// }
+// grandParent();
+
+//! Lexical environment - It is the local memory of function along with lexical environment of the parent
+//! Scope chain - Chain of all the lexical environment and the parent refrence.
+//! Closure - the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). Closures are a result of lexical scoping.
+
+// function counter() {
+//   let count = 0;
+
+//   function increment() {
+//     count++;
+//     console.log(count);
+//   }
+
+//   return increment;
+// }
+// let fn = counter();
+// fn();
+// fn();
+// fn();
+
+//? Whenever function is return from another function, they remembers their lexical environment (they remember where they are actually present)
+
+// function counter() {
+//   let count = 0;
+
+//   return function () {
+//     count++;
+//     return count;
+//   };
+// }
+// let increment1 = counter();
+// console.log(increment1());
+// console.log(increment1());
+// console.log(increment1());
+
+// let increment2=counter()
+// console.log(increment2())
+// console.log(increment2())
+// console.log(increment2())
+
+// function sum(a, b, c) {
+//   return a + b + c;
+// }
+// let op1 = sum(10, 20, 30);
+// console.log(op1);
+// let op2 = sum(10, 20);
+// console.log(op2);
+
+//! Currying
+//? It is a functional programming technique, where a function with multiple arguments, is tranformed into a series of function each taking a single argument.
+
+// function outer(a) {
+//   function middle(b) {
+//     function inner(c) {
+//       return a + b + c;
+//     }
+//     return inner;
+//   }
+//   return middle;
+// }
+// let first = outer(10);
+// console.log(first);
+// let second = first(20);
+// console.log(second);
+// let third = second(30);
+// console.log(third);
+
+// let output=outer(10)(20)(30)
+// console.log(output)
+
+//? Instaed of taking all the arguments at once, the curried function take the first argument and return a new function, which takes the second argument and returns a new function, which takes the third argument and so on, till all the arguments are recived and then it provides you with an output.
+//? In simpler terms, currying breaks down the function taking multiple paramter into a a chain of functions taking single parameter at a time.
+
+//! Generator function
+//? Function that can pause and resume its exuction later is called as a generator function. It yields the value in the process
+
+function* generateNumbers() {
+  yield 1;
+  yield 2;
+  yield 3;
+
+  return "Execution done";
+}
+
+let serial = generateNumbers();
+console.log(serial);
+let a = serial.next().value;
+console.log(a);
+console.log(serial.next().value);
+console.log(serial.next().value);
+console.log(serial.next().value);
