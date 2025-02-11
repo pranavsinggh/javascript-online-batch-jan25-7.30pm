87306f0553b03fe3cee7a1b8fb0b8ef25ccc0321
pranavsinggh <singh167pranav@gmail.com>
2025-02-11 22:07:12 +0530

    asynchronus

--- asynchronus.js
+++ asynchronus.js
@@ -118,19 +118,206 @@
 //? Whenver we return anything from a promise or then, catch, finally it returns a promise only
 //? Either we can a return a promise or we can return a value, if we return a value by default it will be in the resolved state.
 
-const cart = ["Shoes", "Shirts", "Kurtas"];
+// const cart = ["Shoes", "Shirts", "Kurtas"];
 
-createOrderAPI(cart)
-  .then(order => {
-    return proccedToPaymentAPI(order);
-  })
-  .then(paymentInfo => {
-    return showOrderSummaryAPI(paymentInfo);
-  })
-  .then(orderInfo => {
-    return updateWallletBalanceAPI(orderInfo);
-  })
-  .then(data => {
+// createOrderAPI(cart)
+//   .then(order => {
+//     return proccedToPaymentAPI(order);
+//   })
+//   .then(paymentInfo => {
+//     return showOrderSummaryAPI(paymentInfo);
+//   })
+//   .then(orderInfo => {
+//     return updateWallletBalanceAPI(orderInfo);
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(err => console.log(err));
+
+// let success = Math.floor(Math.random() * 10);
+// console.log(success);
+
+// let promise = new Promise((resolve, reject) => {
+//   if (success > 2) {
+//     resolve("1st promise got resolved");
+//   } else {
+//     reject("1st promise got rejected");
+//   }
+// });
+// promise
+//   .then(data => {
+//     console.log(data);
+//     return new Promise((resolve, reject) => {
+//       if (success > 4) {
+//         resolve("2nd promise got resolved");
+//       } else {
+//         reject("2nd promise got rejected");
+//       }
+//     });
+//   })
+//   .then(data => {
+//     console.log(data);
+//     return new Promise((resolve, reject) => {
+//       if (success > 6) {
+//         resolve("3rd promise got resolved");
+//       } else {
+//         reject("3rd promise got rejected");
+//       }
+//     });
+//   })
+//   .then(data => {
+//     console.log(data);
+//     return new Promise((resolve, reject) => {
+//       if (success > 8) {
+//         resolve("4th promise got resolved");
+//       } else {
+//         reject("4th promise got rejected");
+//       }
+//     });
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+//^ Promise methods
+//* resolve() - It will create a promise and immediatley resolve the promise
+//~ Syntax - Promise.resolve(data)
+
+// let p=Promise.resolve("My promise is resolved")
+// console.log(p)
+
+//* reject() - It will create a promise and immediatley reject the promise
+//~ Syntax - Promise.reject(data)
+
+// let p=Promise.reject("My promise is rejected")
+// console.log(p)
+
+// let p1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("1st promise resolved");
+//   }, 2000);
+// });
+
+// let p2 = Promise.resolve("2nd promise got resolved");
+
+// let p3 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     reject("3rd promise got rejected");
+//   }, 1000);
+// });
+
+// let p4 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("4th promise got resolved");
+//   }, 3000);
+// });
+
+// let p5 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     reject("5th promise got rejected");
+//   }, 4000);
+// });
+
+//* all() - It will take an array of promises, and it will wait for all of them to be settled. Then it will check are all the promises fulfilled, if yes then return the resolved data of all the promises, otherwise return the first rejected promise. It will return a promise only, need to consume it.
+//~ Syntax - Promise.all([promise1,promise2,promise3.....])
+
+// Promise.all([p1, p2, p4])
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+//* allSettled() - It will take an array of promises and it will wait for all of them to be settled. Once settled it will return their respective data. It will also return a promise need to consume it
+//~ Syntax - Promise.allSettled([promise1,promise2,promise3.....])
+
+// Promise.allSettled([p1, p2, p3, p4, p5])
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+//* any() - It will return the first resolved promise. If all of the promise are rejecetd, then it will give an error. It will return a promise need to consume it.
+//~ Syntax - Promise.any([promise1,promise2,promise3.....])
+
+// Promise.any([p1, p2, p3, p4, p5])
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+//* race() - It will return the first settled promise. It will return a promise need to comsume it.
+//~ Syntax - Promise.race([promise1,promise2,promise3.....])
+
+// Promise.race([p1,  p3, p4, p5])
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+//& Async/Await
+//? It is modern javascript feature, used to simplify consuming promises, making asynchronus code look more synchronus
+//? async - It will make a function asynchronus, and whatever we return from the function will be considered as a promise.
+//? await - Pauses the execution of an async function, until the promise gets setttled. Can only be used with async
+
+//? Cleaner and more readable code compare to promise chaining. Simplifies error handling
+
+// let promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Promise got resolved");
+//   }, 4000);
+// });
+
+// let fn = async function () {
+//   console.log("start");
+//   try {
+//     let data = await promise;
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error)
+//   }
+//   console.log("end");
+// };
+// fn();
+
+//! fetch
+//? It is a fetch api, which provides an easy to use interface for making your http requests. It is built into modern browsers and allow for asynchronus commuinication with server.
+//? It is used to make api calls.
+//? By default it will return a promise, that we need to consume by using either then/catch or async/await
+//~ Syntax - fetch("url")
+
+// let apiCall = fetch("https://api.github.com/users");
+// apiCall
+//   .then(data => {
+//     console.log(data);
+//     return data.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+let fn = async function () {
+  try {
+    let result = await fetch("https://api.github.com/users");
+    console.log(result);
+    let data = await result.json();
     console.log(data);
-  })
-  .catch(err => console.log(err));
+  } catch (error) {
+    console.log(error);
+  }
+};
+fn();
